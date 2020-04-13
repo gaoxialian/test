dcddbadaa669b68ee12ace1d6335bf66988a6286
gaoxialian <1253555947@qq.com>
2020-04-13 22:28:05 +0800

    testrunn

--- chandao/testrun/runner.py
+++ chandao/testrun/runner.py
@@ -1,18 +1,18 @@
 __author__ = 'Administrator'
-from chandao.testrun import HTMLTestRunner_cn
+import HTMLTestRunner_cn
 import sys
 import time
 import unittest
 import os
-project_dir = r"C:\Users\Administrator\PycharmProjects\test"
+project_dir = "C:\\Users\\Administrator\\PycharmProjects\\test"
 sys.path.append(project_dir)
 
-test_dir = "../testcase"
-test_report = "../testreport"
+test_dir =os.path.join(project_dir,'chandao', 'testcases')
+test_report = os.path.join(project_dir,'chandao', 'testreport')
 
 discover = unittest.defaultTestLoader.discover(test_dir, pattern="test*.py")
 
 reoprt_path = os.path.join(test_report, "report.html")
 with open(reoprt_path, 'wb') as file:
-    run = HTMLTestRunner_cn.HTMLTestRunner(stream=file, title="title",description="描述", save_last_try=2)
+    run = HTMLTestRunner_cn.HTMLTestRunner(stream=file, title="title",description="描述",retry =2)
     run.run(discover)
